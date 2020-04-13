c042b70d0feca1bfe1f36cc70a3f60ba31e94379
Rajeshroulo <rajraval017@gmail.com>
2020-04-13 09:43:07 +0530

    announced 20 days results

---
+++ 20daysvalues.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -x
+
+echo Enter stake,days and goals
+read s
+read d
+read g
+
+bets=1
+wins=0
+loss=0
+
+for(( t=0; t<$d; t++ ))
+do 
+  while(( $s>0 && $s<$g ))
+  do
+    ((bets++))
+     if(( $((1+RANDOM%2)) <= 1 ))
+     then
+         ((s++))
+     else
+         ((s--))
+     fi
+  done
+  if(( $s = $g ))
+  then
+     ((wins++))
+  else
+     ((loss++))
+  fi
+done
+echo total wins are $wins
+echo total loss are $loss
